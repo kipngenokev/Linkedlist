0d5b572f76b5720d4692f03d8ce0b7eab61c7003
brian070069 <kkipngeno409@gmail.com>
2025-02-07 22:56:31 +0300

    first commit

---
+++ src/dev/lpa/Main.java
@@ -0,0 +1,49 @@
+package dev.lpa;
+
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+//        LinkedList<String> placesToVisit = new LinkedList<>();
+        var placesToVisit = new LinkedList<String>();
+        placesToVisit.add("Sidney");
+        placesToVisit.add(0,"Perth");
+        addMoreElements(placesToVisit);
+        System.out.println(placesToVisit);
+
+        removeElements(placesToVisit);
+        System.out.println(placesToVisit);
+    }
+
+    private static void addMoreElements(LinkedList<String> list) {
+        list.addFirst("Darwin");
+        list.addLast("Hobert");
+        list.offer("Melbourne");
+        list.offerFirst("Canberra");
+        list.offerLast("Toowoomba");
+        list.push("Alice Springs");
+    }
+
+    private static void removeElements(LinkedList<String> list) {
+        list.remove(4);
+        list.remove("Hobert");
+
+        String s1 = list.remove();
+        System.out.println(s1 + " was removed");
+
+        String s2 = list.removeFirst();
+        System.out.println(s2 + " was removed");
+
+        String s3 = list.removeLast();
+        System.out.println(s3 + " was removed");
+
+        String p1 = list.poll();
+        System.out.println(p1 + " was removed");
+
+        String p2 = list.pollFirst();
+        System.out.println(p2 + " was removed");
+
+        String p3 = list.pollLast();
+        System.out.println(p3 + "was removed");
+    }
+}
